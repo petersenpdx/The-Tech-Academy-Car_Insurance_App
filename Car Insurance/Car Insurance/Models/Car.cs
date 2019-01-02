fcf2ff9a3ade6983e80ed25112d6d86d3390dc36
Mark Petersen <petersenpdx@gmail.com>
2019-01-01 17:04:34 -0900

    Upload two

---
+++ Car Insurance/Car Insurance/Models/Car.cs
@@ -0,0 +1,41 @@
+﻿namespace CarInsuranceProjApp.Models
+{
+
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Car
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public System.DateTime DateOfBirth { get; set; }
+
+        public int CarYear { get; set; }
+
+        public string CarMake { get; set; }
+
+        public string CarModel { get; set; }
+
+        public int Tickets { get; set; }
+
+        public string Dui { get; set; }
+
+        public string Coverage { get; set; }
+
+        public Nullable<decimal> QuoteTotal { get; set; } // CD: gen'd from new column in db
+                                                          // had to add new column to db to store quote val
+                                                          // ALTER TABLE Cars
+                                                          // ADD QuoteTotal decimal(8,2) null
+                                                          // then updated the edmx from the database
+                                                          // set new QuoteTotal prop to Nullable (.edmx props)
+
+    }
+
+}
---
+++ Car Insurance/Car Insurance/Models/CarsDBModel.Context.cs
@@ -0,0 +1,27 @@
+﻿namespace CarInsuranceProjApp.Models
+{
+
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+
+    public partial class CarsDBEntities1 : DbContext
+    {
+        public CarsDBEntities1()
+            : base("name=CarsDBEntities1")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+
+        public virtual DbSet<Car> Cars { get; set; }
+
+    }
+
+}
